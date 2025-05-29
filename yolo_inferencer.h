448995046b2f91826f08fd0dd71154e07375757b
tju <tony.ju@me.com>
2025-05-29 19:31:48 +0800

    restore yolo

--- yolo_inferencer.h
+++ yolo_inferencer.h
@@ -1,10 +1,11 @@
-// yolo_inferencer.h
 #pragma once
 
 #include <atomic>
 #include <condition_variable>
 #include <mutex>
+#include <opencv2/core.hpp>
 #include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <queue>
 #include <string>
@@ -24,14 +25,10 @@ public:
   ~YoloInferencer();
 
   void infer(const InferenceInput &input);
+  int infer1(const InferenceInput &input);
+  void waitForAllTasks(); // 等待所有任务完成
 
 private:
-  cv::dnn::Net net;
-  std::vector<std::string> class_names;
-  int num_classes = 0;
-  cv::Size input_size = {640, 640};
-  bool initialized = false;
-
   struct InferenceTask {
     std::vector<cv::Mat> frames;
     std::string object_name;
@@ -39,20 +36,27 @@ private:
     int gopIdx;
   };
 
-  std::queue<InferenceTask> task_queue;
-  std::mutex queue_mutex;
-  std::condition_variable cv_task;
-  std::thread worker_thread;
-  std::atomic<bool> running;
-
   void loadClassNamesFromEnv();
   void loadModelFromEnv();
   void processLoop();
   void doInference(const InferenceTask &task);
 
-  cv::Mat letterbox(const cv::Mat &src, const cv::Size &target_size,
-                    int stride = 32,
-                    cv::Scalar color = cv::Scalar(114, 114, 114),
-                    float *scale_out = nullptr,
+  cv::dnn::Net net;
+  bool initialized = false;
+  bool running = false;
+  cv::Size input_size = cv::Size(640, 640);
+  std::vector<std::string> class_names;
+  int num_classes = 0;
+
+  std::queue<InferenceTask> task_queue;
+  std::mutex queue_mutex;
+  std::condition_variable cv_task;
+  std::condition_variable done_cv;
+
+  std::thread worker_thread;
+  std::atomic<int> active_tasks = 0;
+
+  cv::Mat letterbox(const cv::Mat &src, const cv::Size &target_size, int stride,
+                    cv::Scalar color, float *scale_out = nullptr,
                     cv::Point *padding_out = nullptr);
 };
