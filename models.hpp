e7a5a2d7b5632dd6c11c3f2ea3ffd19d680ab28c
tju <tony.ju@me.com>
2025-06-09 15:11:53 +0800

    17

--- models.hpp
+++ models.hpp
@@ -13,6 +13,7 @@ struct InferenceInput {
 // 推理结果结构
 struct InferenceResult {
   std::string info;
+  int type = 10; // 0: detection, 1: classification, etc.
   int taskId;
   int frameIndex;
   int seconds;
@@ -23,6 +24,7 @@ struct InferenceResult {
 // 解码过程信息
 struct TaskDecodeInfo {
   int taskId; // 推理状态信息（可扩展）
+  int type = 20;
   int decoded_frames;
   int remain_frames;
 };
@@ -30,6 +32,7 @@ struct TaskDecodeInfo {
 // 推理过程信息
 struct TaskInferInfo {
   int taskId;
+  int type = 30;
   int completed;
   int remain;
 };
